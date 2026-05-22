f02831e641440014be62e0f9e3a0bd1148315b5b
INFINITY VOID <abhinavkumar710@protonmail.com>
2026-05-22 13:28:17 +0530

    Added Application

--- Application/AuditX V2.2/modules/Functionality/VersionInfo.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class VersionInfo
-    Public Property latest_version As String
-    Public Property download_url As String
-    Public Property mandatory As Boolean
-    Public Property release_notes As String
-End Class
